60e8c1e64e5285fc286987d5d79774e15568b86e
Larry Shen <lcs358@nyu.edu>
2014-07-15 03:02:04 -0400

    week 1 poem assignment

---
+++ Assets/SCRIPTS/ThePoet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThePoet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log( "Hello World" );
+		GetComponent<TextMesh>().text="Today a world was born\nA world of wind and sea\nA world devoid of people\nWandering endlessly";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
